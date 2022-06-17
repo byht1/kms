b4a93f49f65cdaa303c83a02366d88eb726831bf
byht1 <vitalikbyht1@gmail.com>
2022-06-17 14:26:03 +0300

    макет

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// import Swiper from 'swiper';
+// import 'swiper/css';
+
+// const swiper = new Swiper('.swiper', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//     dynamicBullets: true,
+//   },
+//   loop: true,
+
+//   autoplay: {
+//     delay: 5000,
+//   },
+// });
+
+new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  loop: true,
+
+  autoplay: {
+    delay: 5000,
+  },
+});
