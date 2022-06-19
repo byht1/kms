44547eccadf918efdd518f70353e86e404a475d4
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-19 14:27:28 +0000

    Deploying to gh-pages from @ byht1/kms@b56a33cbcce56e232f6bbf0fd0581850219ef1a9 🚀

---
+++ index.936ffd78.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",(()=>{document.querySelector(".header").classList.toggle("sticky",window.scrollY>0)}));const e=document.querySelector("nav");document.querySelector(".toggle").onclick=function(){this.classList.toggle("active"),e.classList.toggle("active")},setTimeout((()=>{window.scrollTo(0,100)}),2e3);
+//# sourceMappingURL=index.936ffd78.js.map
---
+++ index.e7a373b2.js
@@ -0,0 +1,2 @@
+!function(){window.addEventListener("scroll",(function(){document.querySelector(".header").classList.toggle("sticky",window.scrollY>0)}));var e=document.querySelector("nav");document.querySelector(".toggle").onclick=function(){this.classList.toggle("active"),e.classList.toggle("active")},setTimeout((function(){window.scrollTo(0,100)}),2e3)}();
+//# sourceMappingURL=index.e7a373b2.js.map
