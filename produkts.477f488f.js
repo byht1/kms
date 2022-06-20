d18920f6a53e83857df1aa8f898b7bb081c6cd60
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-20 09:56:12 +0000

    Deploying to gh-pages from @ byht1/kms@083bb7f01d94fbe910bd0c1237ff634a21f966d7 🚀

---
+++ produkts.477f488f.js
@@ -0,0 +1,2 @@
+!function(){var e=0;0===e&&(window.addEventListener("scroll",(function(){document.querySelector(".header").classList.add("sticky",window.scrollY>0)})),e+=1);var t=document.querySelector("nav");document.querySelector(".toggle").onclick=function(){this.classList.toggle("active"),t.classList.toggle("active")},window.scrollTo(0,1)}();
+//# sourceMappingURL=produkts.477f488f.js.map
---
+++ produkts.79475ca5.js
@@ -0,0 +1,2 @@
+let e=0;0===e&&(window.addEventListener("scroll",(()=>{document.querySelector(".header").classList.add("sticky",window.scrollY>0)})),e+=1);const t=document.querySelector("nav");document.querySelector(".toggle").onclick=function(){this.classList.toggle("active"),t.classList.toggle("active")},window.scrollTo(0,1);
+//# sourceMappingURL=produkts.79475ca5.js.map
