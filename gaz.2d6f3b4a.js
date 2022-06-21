5ecf5ae5bb35cda2728d73279b9b7678ee6ed030
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-21 14:58:10 +0000

    Deploying to gh-pages from @ byht1/kms@18d7e2d55ad4b3b90db1c43d080019abe67bb3a9 🚀

--- gaz.2d6f3b4a.js
+++ gaz.2d6f3b4a.js
@@ -1,2 +1,2 @@
 const t=document.querySelector("nav");document.querySelector(".toggle").onclick=function(){this.classList.toggle("active"),t.classList.toggle("active")},window.scrollTo(0,1);
-//# sourceMappingURL=produkts.e0dec472.js.map
+//# sourceMappingURL=gaz.2d6f3b4a.js.map
