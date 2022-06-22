ddf95fb466cf5a151a12792cc225917f0123af3d
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-22 11:33:02 +0000

    Deploying to gh-pages from @ byht1/kms@10f04629911cd4be26f00c179d5cfbde9237a589 🚀

--- servis.8565be23.js
+++ servis.8565be23.js
@@ -1,2 +1,2 @@
 !function(){var t=document.querySelector("nav");document.querySelector(".toggle").onclick=function(){this.classList.toggle("active"),t.classList.toggle("active")},window.scrollTo(0,1)}();
-//# sourceMappingURL=gaz.995635cb.js.map
+//# sourceMappingURL=servis.8565be23.js.map
