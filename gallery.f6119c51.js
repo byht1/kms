26a2499a03e4d80a611347fc3a01f8d1f5bc9d3c
byht1 <99541944+byht1@users.noreply.github.com>
2022-06-22 13:03:38 +0000

    Deploying to gh-pages from @ byht1/kms@140d2f5b3a2cda7a74644b7ab14b7982b50d9dfc 🚀

--- gallery.f6119c51.js
+++ gallery.f6119c51.js
@@ -1,2 +1,2 @@
 !function(){var t=document.querySelector("nav");document.querySelector(".toggle").onclick=function(){this.classList.toggle("active"),t.classList.toggle("active")},window.scrollTo(0,1)}();
-//# sourceMappingURL=servis.8565be23.js.map
+//# sourceMappingURL=gallery.f6119c51.js.map
